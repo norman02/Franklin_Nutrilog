bbc3e38a7d3f6015fd227ae393d3faf3a7a25eb0
John N. Norman <normanj777@gmail.com>
2019-06-12 15:56:33 -0400

    bug fixes in SQL file

--- Infant Nurtition Log/data.sql
+++ Infant Nurtition Log/data.sql
@@ -5,48 +5,47 @@ CREATE DATABASE data;
 USE data;
 
 CREATE TABLE Person (
-	Id INTEGER,
-	FirstName VARCHAR(50),
-	LastName VARCHAR(50),
-	Gender CHAR);
+	Id INTEGER NOT NULL,
+	FirstName VARCHAR(255),
+	LastName VARCHAR(255) NOT NULL,
+	Gender CHAR,
+	PRIMARY KEY(Id)
+);
 
-CREATE TABLE PatientEvent
-	(Id INTEGER,
+CREATE TABLE PatientEvent(
+	Id INTEGER NOT NULL,
 	EventNumber INTEGER,
-	DateAndTime DATETIME);
+	DateAndTime DATETIME
+	
+);
 
-CREATE TABLE Patient
-	(Id INTEGER,
+CREATE TABLE Patient(
+	Id INTEGER,
 	Weight FLOAT,
-	DOB DATE);
+	DOB DATE
+	
+);
 
 CREATE TABLE Event(
 	EventNumber INTEGER,
-	TypeCode INTEGER);
+	TypeCode INTEGER
+);
 
 CREATE TABLE Feeding(
 	TypeCode INTEGER,
 	Aumount FLOAT,
 	units FLOAT
-	);
+);
 
 CREATE TABLE Weighing(
 	TypeCode INTEGER,
-	Weight FLOAT);
+	Weight FLOAT
+);
 
 CREATE TABLE Changing(
 	TypeCode INTEGER,
 	urine FLOAT,
 	stool FLOAT
-	);
+);
 
-SHOW TABLES;
-
-DESC person;
-
-DESC Changing;
-DESC Event;
-DESC Feeding;
-DESC Patient;
 DESC PatientEvent;
-DESC Weighing;
