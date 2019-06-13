eaf98f23502c70fe17b8d79de267e016bed3be05
John N. Norman <normanj777@gmail.com>
2019-06-13 17:23:46 -0400

    Error testing and foreign keys re-added

--- Infant Nurtition Log/data.sql
+++ Infant Nurtition Log/data.sql
@@ -11,85 +11,54 @@ USE data;
 
 
 CREATE TABLE Person (
-
 	Id INTEGER(9) NOT NULL,
-	
 	FirstName VARCHAR(255),
-	
 	LastName VARCHAR(255) NOT NULL,
-	
 	Gender CHAR,
-	
 	PRIMARY KEY(Id)
-
 );
 
 
 
 CREATE TABLE Event(
-	
+EventCode INTEGER NOT NULL,
 	EventNumber INTEGER NOT NULL,
-	
-	EventCode INTEGER NOT NULL,
-	
 	PRIMARY KEY(EventNumber)
 );
 
-
-
-CREATE TABLE PatientEvent(
-	
-	Id INTEGER NOT NULL,
-	
-	EventNumber INTEGER,
-	
-	DateAndTime DATETIME
-	
+CREATE TABLE PatientEvent(	
+	Id INTEGER NOT NULL,	
+	EventCode INTEGER,	
+	DateAndTime DATETIME	
 );
 
-CREATE TABLE Patient(
-	
-	Id INTEGER,
-	
-	Weight FLOAT,
-	
+CREATE TABLE Patient(	
+	Id INTEGER,	
+	Weight FLOAT,	
 	DOB DATE 	
-
 );
-
-
-
-
 
 CREATE TABLE Feeding(
-
 	EventCode INTEGER,
-	
 	Aumount FLOAT,
-	
 	units FLOAT
 );
 
-
-
 CREATE TABLE Weighing(
-	
 	EventCode INTEGER,
-	
 	Weight FLOAT
 );
 
-
-
 CREATE TABLE Changing(
-	
 	EventCode INTEGER,
-	
-	urine FLOAT,
-	
-	stool FLOAT
+	urine VARCHAR(255),
+	stool VARCHAR(255)
 );
 
+ALTER TABLE Patient
+ADD FOREIGN KEY (id) REFERENCES Person (id);
 
+ALTER TABLE PatientEvent
+ADD FOREIGN KEY (id) REFERENCES Patient (Id);
 
 
