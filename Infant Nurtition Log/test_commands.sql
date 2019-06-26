8144849d3f986ba14c1ffeb3ae8142f5c91594dd
John N. Norman <normanj777@gmail.com>
2019-06-26 17:37:15 -0400

    added tests for searching for baby by name or id

--- Infant Nurtition Log/test_commands.sql
+++ Infant Nurtition Log/test_commands.sql
@@ -8,4 +8,14 @@ VALUES (1, 10, NULL);
 
 #Search for a baby - By last name or By regitraition number
 
+SELECT Person.id, Person.FirstName, Person.LastName, Person.Gender, Patient.Weight, Patient.DOB
+FROM Person
+LEFT JOIN Patient ON Person.id=Patient.id
+WHERE LastName = 'Doe';
+
+SELECT Person.id, Person.FirstName, Person.LastName, Person.Gender, Patient.Weight, Patient.DOB
+FROM Person
+LEFT JOIN Patient ON Person.id=Patient.id
+WHERE Person.id = 1;
+
 #Log an Event - feeding/ changing/ weighing
