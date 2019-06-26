4cdad44df4bd27787af7b658c134a5e5a698a561
John N. Norman <normanj777@gmail.com>
2019-06-26 17:16:44 -0400

    added test for registering a baby

---
+++ Infant Nurtition Log/test_commands.sql
@@ -0,0 +1,11 @@
+#Register a baby
+
+INSERT INTO Person (id, FirstName, LastName, Gender)
+VALUES (1, "John", "Doe", 'M');
+
+INSERT INTO Patient (id, Weight, DOB)
+VALUES (1, 10, NULL);
+
+#Search for a baby - By last name or By regitraition number
+
+#Log an Event - feeding/ changing/ weighing
