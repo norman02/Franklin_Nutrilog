a9ca93e6fb35bc77b1b66541c6e13431e01995cb
John N. Norman <normanj777@gmail.com>
2019-06-27 14:32:41 -0400

    updated ERD and bug fix for test_commands

--- Infant Nurtition Log/test_commands.sql
+++ Infant Nurtition Log/test_commands.sql
@@ -1,4 +1,4 @@
-#Register a baby
+
 
 INSERT INTO Person (id, FirstName, LastName, Gender)
 VALUES (1, "John", "Doe", 'M');
@@ -6,7 +6,6 @@ VALUES (1, "John", "Doe", 'M');
 INSERT INTO Patient (id, Weight, DOB)
 VALUES (1, 10, NULL);
 
-#Search for a baby - By last name or By regitraition number
 
 SELECT Person.id, Person.FirstName, Person.LastName, Person.Gender, Patient.Weight, Patient.DOB
 FROM Person
