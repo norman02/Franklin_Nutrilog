e9f62f446192ced5dea56ed7cd32111bad0f0f65
John N. Norman <normanj777@gmail.com>
2019-07-01 12:12:39 -0400

    Java Interface initial commit

---
+++ Infant Nurtition Log/Nutrilog Database/src/Command.java
@@ -0,0 +1,16 @@
+
+public class Command {
+    
+    //Register a baby
+    
+    //Log Feeding
+    
+    //Log Changing
+    
+    //Log Weighing
+    
+    //Search by last name
+    
+    //Search by registration number
+
+}
