97a34aa714f11548194abae2e39dac3e59a35bc4
John N. Norman <normanj777@gmail.com>
2019-07-09 13:23:07 -0400

    restore to prior state

---
+++ Infant Nurtition Log/Nutrilog DB/src/nutrilog/command.java
@@ -0,0 +1,28 @@
+package nutrilog;
+
+public class command {
+
+    // Information page controller
+    /*
+     * SELECT
+     * FROM
+     * event
+     * LEFT JOIN
+     * Person ON Person.id = Event.Personid;
+     */
+    
+    /*
+     *Search Function Controller
+     * "SELECT * FROM person
+     *WHERE Person.id = ?;"
+
+    " SELECT * FROM person
+    WHERE LastName = ?;"*
+     */
+    
+    /*
+     * Create a Controller that adds a new entry to the Person table. Use the following command as a baseline:
+
+"INSERT INTO Person (id, FirstName, LastName, Gender)"git ch
+     */
+}
