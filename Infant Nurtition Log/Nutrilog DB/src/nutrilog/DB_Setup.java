109a0a786ad2b24d8a58f1b28c511a675605f9c8
John N. Norman <normanj777@gmail.com>
2019-07-27 12:40:40 -0400

    Javadocs added and code clean up for DB_Setup

--- Infant Nurtition Log/Nutrilog DB/src/nutrilog/DB_Setup.java
+++ Infant Nurtition Log/Nutrilog DB/src/nutrilog/DB_Setup.java
@@ -1,26 +1,44 @@
 package nutrilog;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+/**
+ * Creates initializes and deletes the database Nutrilog_Data
+ * @author John Norman
+ * @version 2019-7-27
+ *
+ */
 public class DB_Setup{
-    // JDBC driver name and database URL
-    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost/";
-
-    // Database credentials
-    static final String USER = "root";
-    static final String PASS = "";
-    Connection conn;
-    Statement stmt;
-
+    // TODO abstract this
     /**
-     * Initializes the Nutrilog database
-     * @return 
-     * 
-     * @throws SQLException
+     * Path for the JDBC Driver
+     */
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    /**
+     * Path to the local host
+     */
+    static final String DB_URL = "jdbc:mysql://localhost/";
+    /**
+     * Username for the database
+     */
+    static final String USER = "root";
+    /**
+     * User password for the database
+     */
+    static final String PASS = "";
+    /**
+     * A connection to the database
+     */
+    Connection conn;
+    /**
+     * A statement for SQL queries
+     */
+    Statement stmt;
+    /**
+     * Initializes the Nutrilog database. Creating the tables and 
+     * assigning key constraints.
+     * @return boolean representing if the database is successfully initialized
      */
     boolean init(){
         try {
@@ -28,6 +46,7 @@ public class DB_Setup{
             conn = connect("jdbc:mysql://localhost/Nutrilog_Data",USER,PASS);
             // Execute a query
             stmt = conn.createStatement();
+            // Key restraints disabled for development TOODO: restore keys
             // Create Person Table
             sql = "CREATE TABLE Person(" 
                     + "    ID INTEGER NOT NULL,"
@@ -38,22 +57,17 @@ public class DB_Setup{
                     + "    Weight FLOAT,"
                     + "    PRIMARY KEY(ID)" 
                     + ")";
-            stmt.executeUpdate(sql);
-            
-            // Create Application User table
-            
+            stmt.executeUpdate(sql);            
+            // Create Application User table           
             sql = "CREATE TABLE AppUser(" + "    ID INTEGER NOT NULL, "
                     + "    UserName VARCHAR(255) NOT NULL, "
                     + "    Password VARCHAR(255) NOT NULL "
-
 //                    + "    PRIMARY KEY (ID),"
 //                    + "    FOREIGN KEY (ID) REFERENCES Person(Id)" 
                     + ")"
                     ;
-            stmt.executeUpdate(sql);
-           
-            // Create Patient table
-            
+            stmt.executeUpdate(sql);          
+            // Create Patient table            
             sql = "CREATE TABLE Patient("
                     + "    ID INTEGER NOT NULL, "
                     + "    LastName VARCHAR(255), "
@@ -61,10 +75,8 @@ public class DB_Setup{
                     + "    PRIMARY KEY (ID)"
 //                    + "    FOREIGN KEY (ID) REFERENCES Person (ID)\r\n"
                     + ")";
-            stmt.executeUpdate(sql);
-            
-            // Create Event table
-            
+            stmt.executeUpdate(sql);          
+            // Create Event table            
             sql = "CREATE TABLE Event(\r\n"
                     + "    EventNum INTEGER NOT NULL,\r\n"
                     + "    ID INTEGER NOT NULL,\r\n"
@@ -77,8 +89,7 @@ public class DB_Setup{
                     + "    PRIMARY KEY (EventNum)\r\n"
  //                   + "    ,FOREIGN KEY (ID) REFERENCES Patient(ID)\r\n"
                     + ")";
-            stmt.executeUpdate(sql);
-            
+            stmt.executeUpdate(sql);            
             return true;
         } catch (SQLException e) {
             e.printStackTrace();
@@ -87,76 +98,55 @@ public class DB_Setup{
             close();
         }
         return false;
-
     }
-
-
-
     /**
      * Creates the database Nutrilog_Data
-     * @return 
-     * 
-     * @throws SQLException
+     * @return boolean representing if database is successfully created
      */
     boolean create() {
-        try {
-            
-            conn = connect(DB_URL, USER, PASS);
-            
+        try {         
+            conn = connect(DB_URL, USER, PASS);            
             // Execute a query
-            stmt = conn.createStatement();
-            
+            stmt = conn.createStatement();            
             String sql = "CREATE DATABASE NUTRILOG_DATA";
-            stmt.executeUpdate(sql);
-            
+            stmt.executeUpdate(sql);            
             return true;
-        } catch (SQLException e) {
-            
+        } catch (SQLException e) {            
             e.printStackTrace();
         } finally {
             // close resources
-            try {
-                
-                conn.close();
-                
-            } catch (SQLException e) {
-                
+            try {                
+                conn.close();               
+            } catch (SQLException e) {                
                 e.printStackTrace();
             }
         }
         return false;
-
     }
-
     /**
-     * Deletes Nutrilog_Data to create a fresh test run
-     * @return 
-     * 
-     * @throws SQLException
+     * Deletes database Nutrilog_Data
+     * @return boolean representing deletion successful 
      */
     boolean dropDatabase(){
-        try {
-            
+        try {            
             conn = connect(DB_URL, USER, PASS);            
             // Execute a query
             stmt = conn.createStatement();
             String sql = "DROP DATABASE Nutrilog_Data";
             stmt.executeUpdate(sql);
             return true;
-        } catch (SQLException e) {
-            
+        } catch (SQLException e) {            
             e.printStackTrace();
         } finally {
          // close resources
             close();
         }
         return false;
-        
-
     }
-
-    private void close() {
-        
+    /**
+     * Closes the connect and statment
+     */
+    private void close() {        
         try {
             if (stmt != null)
                 conn.close();
@@ -167,12 +157,16 @@ public class DB_Setup{
                 conn.close();
         } catch (SQLException se) {
             se.printStackTrace();
-        } // end close resources
-        
+        } // end close resources        
     }
-
+    /**
+     * Creates a connection to the database
+     * @param url - path to the database
+     * @param user - username
+     * @param password
+     * @return the connection
+     */
     private Connection connect(String url, String user, String password) {
-
         try {
             // Register JDBC driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -186,4 +180,4 @@ public class DB_Setup{
         }
         return conn;
     }
-}//
+}
--- Infant Nurtition Log/Nutrilog DB/src/nutrilog/DB_SetupTest.java
+++ Infant Nurtition Log/Nutrilog DB/src/nutrilog/DB_SetupTest.java
@@ -1,43 +1,65 @@
 package nutrilog;
-
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+/**
+ * A JUnit test class for the DB_Setup class
+ * @author John Norman
+ * @version 2019-7-27
+ */
 public class DB_SetupTest {
+    /**
+     * Instantiate a test case for DB_Setup
+     */
     DB_Setup db;
-
+    /**
+     * Before each test create a new database
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
         db = new DB_Setup();
         db.create();
     }
-
+    /**
+     * After each test delete the database
+     * @throws Exception
+     */
     @After
     public void tearDown() throws Exception {
         db.dropDatabase();
     }
+    /**
+     * Test the default constructor method is not null
+     */
     @Test
     public void testDefaultConstructor() {
         assertNotNull(db);
     }
+    /**
+     * Test the init() method returns true on a successful initialization
+     * TODO Test False
+     */
     @Test
-    public void testInit() {
-        
+    public void testInit() {        
         assertTrue(db.init());
     }
-
+    /**
+     * Test the Create() method returns true on a successful creation
+     * TODO Test False
+     */
     @Test
     public void testCreate() {
        db.dropDatabase();
        assertTrue(db.create());
     }
-
+    /**
+     * Test that DropDatabase() returns true on a successful deletion
+     * TODO test False
+     */
     @Test
     public void testDropDatabase() {
         assertTrue(db.dropDatabase());
     }
-
 }
